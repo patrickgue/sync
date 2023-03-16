b12047fd869fb3ef18fd751debb83422f195d141
Patrick Günthard <patrick.guenthard@helmedica.com>
2023-03-16 11:15:52 +0100

    build libssh2 against openssl

--- setup.sh
+++ setup.sh
@@ -3,13 +3,21 @@ rm -rf lib
 mkdir -p lib/opt
 cd lib
 
-wget https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.10.1.tar.bz2
-tar xfvz libgcrypt-1.10.1.tar.bz2
+#wget https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.10.1.tar.bz2
+#tar xfvz libgcrypt-1.10.1.tar.bz2
 
-mkdir libgcrypt-build
-cd libgcrypt-build
-../libgcrypt-1.10.1/configure --prefix=$PWD/../opt/
-make
+#mkdir libgcrypt-build
+#cd libgcrypt-build
+#../libgcrypt-1.10.1/configure --prefix=$PWD/../opt/
+#make
+#make install
+
+wget https://www.openssl.org/source/openssl-3.1.0.tar.gz
+tar xfvz openssl-3.1.0.tar.gz
+mkdir openssl-build
+cd openssl-build
+../openssl-3.1.0/Configure --prefix=$PWD/../opt
+make -j 4
 make install
 
 cd ../
@@ -20,6 +28,6 @@ tar xfvz libssh2-1.10.0.tar.gz
 
 mkdir libssh2-build
 cd libssh2-build
-../libssh2-1.10.0/configure --with-crypto=libgcrypt --prefix=$PWD/../opt/
-make
+../libssh2-1.10.0/configure --with-crypto=openssl --prefix=$PWD/../opt/
+make -j 4
 make install
